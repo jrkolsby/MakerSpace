191317dadfe8747a75a468d48a90b94c27e4a926
James Kolsby <jrkolsby@mac.com>
2014-05-24 12:16:34 -0400

    Finished header

--- js/header.js
+++ js/header.js
@@ -1,11 +1,19 @@
-var openMenu = function(menuId) {
-		
-		//Slide down the correct menu block
-	};
+var openMenu = function(ref) {
+	if (!$('.largeMenu#'+ ref).hasClass('visible') && $('.largeMenu.visible').length == 0) {
+		$('.largeMenu#'+ ref).addClass('visible');
+	} else if (!$('.largeMenu#'+ ref).hasClass('visible')) {
+		$('.largeMenu.visible').removeClass('visible');
+		setTimeout(function() {
+			$('.largeMenu#'+ ref).addClass('visible');
+		}, 300);
+	} else {
+		$('.largeMenu.visible').removeClass('visible');
+	}
+}
 $.fn.initMenu = function(menuObject) {
 	for (var key in menuObject) {
 		var largeMenu = $('<div></div>').addClass('largeMenu').attr('id', key).insertAfter($(this))
-			largeMenuWrapper = $('<div></div>').addClass('largeMenuWrapper').appendTo(largeMenu)
+			largeMenuWrapper = $('<div></div>').addClass('largeMenuWrapper').appendTo(largeMenu);
 		for (var i=0;i<menuObject[key].length;i++) {
 			var largeMenuGroup = $('<div></div>').addClass('largeMenuGroup').appendTo(largeMenuWrapper);
 			$('<h3></h3>').append(menuObject[key][i]['groupName']).appendTo(largeMenuGroup);
@@ -22,11 +30,6 @@ $.fn.initMenu = function(menuObject) {
 };
 $(document).ready(function() {
 	$('nav span').click(function() {
-		if (!$('.largeMenu#'+$(this).attr('ref')).hasClass('visible')) {
-			$('.largeMenu.visible').removeClass('visible');
-			$('.largeMenu#'+$(this).attr('ref')).addClass('visible');
-		} else {
-			$('.largeMenu.visible').removeClass('visible');
-		}
+		openMenu($(this).attr('ref'));
 	});
 });
