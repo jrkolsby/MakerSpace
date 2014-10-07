fd2ee77a5c80dcc119a24e933e0a4e124318a631
James Kolsby <jrkolsby@mac.com>
2014-10-06 21:06:17 -0400

    Added mail frontend

---
+++ makersclub/mail/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+		<title>Mail</title>
+		<link rel="stylesheet" type="text/css" href="css/html5.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<script type='text/javascript' src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="js/main.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<header>
+			<div id="logo"></div>
+		</header>
+		<section id="container">
+			<section id="members">
+				<h1>Member List</h1>
+				<div id="people">
+					<div class="person" contenteditable="TRUE">John Appleseed</div>
+					<div class="person" contenteditable="TRUE">Lorem Jones</div>
+					<div id="add">+</div>
+				</div>
+			</section>
+			<section id="compose">
+				<h1>Compose Email</h1>
+				<textarea></textarea>
+				<button>Send</button>
+			</section>
+		</section>
+	</body>
+</html>
---
+++ makersclub/mail/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#add").click(function(){
+		alert("add");
+	})
+	$(".recip").hover(function(){
+		//alert("show x to delete member");
+	})
+});
